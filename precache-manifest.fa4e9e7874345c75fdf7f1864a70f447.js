1a062671f2bd9beee990b8f204c031927313b11f
chintanmodi92 <48188541+chintanmodi92@users.noreply.github.com>
2020-01-12 11:25:59 -0500

    Updates

---
+++ precache-manifest.fa4e9e7874345c75fdf7f1864a70f447.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a53ee76ad5983ad7539701728ece69b8",
+    "url": "/shopping-react-redux/index.html"
+  },
+  {
+    "revision": "b6603b82d9d43606dcf1",
+    "url": "/shopping-react-redux/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7037780a26490cb9d2fb",
+    "url": "/shopping-react-redux/static/js/2.422505e0.chunk.js"
+  },
+  {
+    "revision": "d9801c977a8a9f6f6b72aac1b777531b",
+    "url": "/shopping-react-redux/static/js/2.422505e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b6603b82d9d43606dcf1",
+    "url": "/shopping-react-redux/static/js/main.89c9374f.chunk.js"
+  },
+  {
+    "revision": "a1c86df78471d0c46a64",
+    "url": "/shopping-react-redux/static/js/runtime-main.ba17a367.js"
+  },
+  {
+    "revision": "50637965c816e91c267d26780b3a9acc",
+    "url": "/shopping-react-redux/static/media/item1.50637965.jpg"
+  },
+  {
+    "revision": "76799c73d9934535a3b691d96e40135a",
+    "url": "/shopping-react-redux/static/media/item2.76799c73.jpg"
+  },
+  {
+    "revision": "c3a017c14a9fb1caec6ca13b9c3e46f4",
+    "url": "/shopping-react-redux/static/media/item3.c3a017c1.jpg"
+  },
+  {
+    "revision": "ec07868b11f508b216bc47420d7c9859",
+    "url": "/shopping-react-redux/static/media/item4.ec07868b.jpg"
+  },
+  {
+    "revision": "036fecdefaf24a27b4d180b95761de6a",
+    "url": "/shopping-react-redux/static/media/item5.036fecde.jpg"
+  },
+  {
+    "revision": "2a1ce7f6f2926b48e5729fff71f7f942",
+    "url": "/shopping-react-redux/static/media/item6.2a1ce7f6.jpg"
+  }
+]);
